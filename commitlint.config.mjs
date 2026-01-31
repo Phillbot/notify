c13c5e234cbb01d75e42502f16a802239be4810e
phillbot <d.philippenko@gmail.com>
2026-02-01 00:40:19 +0200

    Improvement: #common Add subject-case rule to commitlint config

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -14,6 +14,7 @@ export default {
     "type-case": [2, "always", "pascal-case"], // Enforce PascalCase for type
     "scope-enum": [2, "always", ["common", "web", "mobile", "server", "desktop", "core"]],
     "subject-empty": [2, "never"],
+    "subject-case": [2, "always", "sentence-case"],
     "header-max-length": [2, "always", 100],
   },
 };
