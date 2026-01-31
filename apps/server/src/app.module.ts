b54139dbc4423283fc86eff6a4ef47d52a521347
phillbot <d.philippenko@gmail.com>
2026-01-31 10:20:24 +0200

    Improvement: #server Refactor structure and move files to src

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { AppGateway } from './app.gateway';
+import { AppController } from '@/app.controller';
+import { AppService } from '@/app.service';
+import { AppGateway } from '@/app.gateway';
 
 @Module({
   imports: [],
