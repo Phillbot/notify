9ceb2def3bfd614524c48243e4ae1b7473010f58
phillbot <d.philippenko@gmail.com>
2026-02-01 00:39:11 +0200

    Fix: #mobile Restore full Babel config for decorators and declare fields

--- apps/mobile/babel.config.cjs
+++ apps/mobile/babel.config.cjs
@@ -2,9 +2,33 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
+    assumptions: {
+      setPublicClassFields: true,
+      privateFieldsAsProperties: true,
+    },
     plugins: [
       "babel-plugin-transform-typescript-metadata",
+      [
+        require.resolve("@babel/plugin-transform-typescript"),
+        { allowDeclareFields: true },
+      ],
       ["@babel/plugin-proposal-decorators", { legacy: true }],
+      [
+        require.resolve("@babel/plugin-transform-flow-strip-types"),
+        { allowDeclareFields: true },
+      ],
+      [
+        require.resolve("@babel/plugin-transform-class-properties"),
+        { loose: true },
+      ],
+      [
+        require.resolve("@babel/plugin-transform-private-methods"),
+        { loose: true },
+      ],
+      [
+        require.resolve("@babel/plugin-transform-private-property-in-object"),
+        { loose: true },
+      ],
       [
         "module-resolver",
         {
