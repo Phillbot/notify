643c93dd53b73e996e8e97610feb94af2b93a080
Phillbot <d.philippenko@gmail.com>
2026-02-01 23:01:27 +0200

    Feature: #desktop Enforce window constraints and add branding

--- apps/desktop/index.js
+++ apps/desktop/index.js
@@ -1,14 +1,20 @@
 import path from "path";
+import fs from "fs";
 import { fileURLToPath } from "url";
 
 import { app, BrowserWindow } from "electron";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+const pkg = JSON.parse(fs.readFileSync(path.join(__dirname, "package.json"), "utf8"));
+
 function createWindow() {
   const win = new BrowserWindow({
     width: 1200,
     height: 800,
+    minWidth: 400,
+    minHeight: 500,
+    title: `${pkg.productName} v${pkg.version}`,
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
