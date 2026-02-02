252b116eed0e2a54aefa4f39698557dda4febf73
Phillbot <d.philippenko@gmail.com>
2026-02-02 16:49:24 +0200

    Feature: #web Add TitleBar component to app layout

--- apps/web/src/app/app.component.tsx
+++ apps/web/src/app/app.component.tsx
@@ -1,6 +1,7 @@
 import { PureComponent } from "react";
 
 import { Chat } from "@/features/chat";
+import { TitleBar } from "@/features/desktop/ui/title-bar/title-bar.component";
 
 import styles from "./app.module.scss";
 
@@ -8,6 +9,7 @@ export class App extends PureComponent {
   override render() {
     return (
       <div className={styles.app}>
+        <TitleBar />
         <Chat />
       </div>
     );
