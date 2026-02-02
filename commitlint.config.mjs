1479d8cf13885ec7bb26f11b3a77f8cb544e4994
Phillbot <d.philippenko@gmail.com>
2026-02-02 11:55:57 +0200

    Style: #common Format commitlint configuration file

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -9,7 +9,18 @@ export default {
     "type-enum": [
       2,
       "always",
-      ["Feature", "Fix", "Hotfix", "Improvement", "Docs", "Style", "Refactor", "Test", "Chore", "Revert"],
+      [
+        "Feature",
+        "Fix",
+        "Hotfix",
+        "Improvement",
+        "Docs",
+        "Style",
+        "Refactor",
+        "Test",
+        "Chore",
+        "Revert",
+      ],
     ],
     "type-case": [2, "always", "pascal-case"], // Enforce PascalCase for type
     "scope-enum": [2, "always", ["common", "web", "mobile", "server", "desktop", "core"]],
